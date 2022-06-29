4851d1c3e7a36903b2a3623974ce1a0e61d7ff2d
akshay_ashok <akshayashokdj@gmail.com>
2022-06-29 19:22:10 +0530

    improved

--- index.js
+++ index.js
@@ -22,49 +22,29 @@ document.addEventListener("keypress", function(event) {
 
 });
 
+let audioMap = {
+w: new Audio("sounds/tom-1.mp3"),
+a: new Audio("sounds/tom-2.mp3"),
+s: new Audio('sounds/tom-3.mp3'),
+d: new Audio('sounds/tom-4.mp3'),
+j:  new Audio('sounds/snare.mp3'),
+k: new Audio('sounds/crash.mp3'),
+l: new Audio('sounds/kick-bass.mp3')
+}
+
+for (const [key, sound] of Object.entries(audioMap)) {
+  sound.load();
+}
 
 function makeSound(key) {
 
-  switch (key) {
-    case "w":
-      let tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "s":
-      let tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
-      break;
-
-    case "d":
-      let tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
-      break;
-
-    case "j":
-      let snare = new Audio('sounds/snare.mp3');
-      snare.play();
-      break;
-
-    case "k":
-      let crash = new Audio('sounds/crash.mp3');
-      crash.play();
-      break;
-
-    case "l":
-      let kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
-      break;
-
-
-    default: console.log(key);
-
+  let sound = audioMap[key];
+  if(sound) {
+    sound.play();
+  } else {
+    console.log("invalid key:" + key);
   }
+ 
 }
 
 
